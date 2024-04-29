6868a7adab67451447e9ce108fbad8488a62ce54
jzhang3496 <jzhang3496@gatech.edu>
2024-04-29 10:46:22 +0200

    create an interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form>
@@ -14,8 +18,19 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
       <app-housing-location></app-housing-location>
     </section>
   `,
-  styleUrl: './home.component.css',
-  standalone: true,
-  imports: [HousingLocationComponent],
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
