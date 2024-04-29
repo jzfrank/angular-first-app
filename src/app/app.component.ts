5848a0db57e9dde2d1186fbdf072d6092b7f7503
jzhang3496 <jzhang3496@gatech.edu>
2024-04-29 09:42:06 +0200

    add home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   styleUrl: './app.component.css',
-  template: `<h1>Hello world!</h1>`,
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
 })
 export class AppComponent {
-  title = 'homes';
+  title = 'hi';
 }
